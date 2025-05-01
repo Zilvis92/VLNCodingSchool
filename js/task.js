94ab2a1e2006e548ff17e1fcdc83fb5fbeaab4da
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-05-01 21:57:40 +0300

    if else uzduotys

--- js/task.js
+++ js/task.js
@@ -7,12 +7,29 @@
 // console.log('| Juste  |   25   |');
 // console.log('+--------+--------+');
 
-console.log(
-    '+--------+--------+\n' +
-    '| Vardas | Amzius |\n' +
-    '+--------+--------+\n' +
-    '| Tomas  |   25   |\n' +
-    '| Jonas  |   26   |\n' +
-    '| Juste  |   25   |\n' +
-    '+--------+--------+'
-  );
+// console.log(
+//     '+--------+--------+\n' +
+//     '| Vardas | Amzius |\n' +
+//     '+--------+--------+\n' +
+//     '| Tomas  |   25   |\n' +
+//     '| Jonas  |   26   |\n' +
+//     '| Juste  |   25   |\n' +
+//     '+--------+--------+'
+//   );
+
+// task 8.  data:25/05/01
+// (sunkesne) Susikurkite dvizenkli skaiciu. Raskite jo skaitmeny suma.
+// Pavyzdziui: 58 -> 5 + 8 = 13.
+// Kad gauti pirma skaitmeni galima dalinti is 10 (ir panaudoti
+// Math.floor(skaicius) arbaMath.ceil(skaicius)).
+// Kad gauti antra skaitmeni galima dalinti su % is 10.
+console.log(Math.floor(58 / 10) + (58 % 10));
+console.log('---------------------------------------------------------------------');
+// Dabar pagal tas pacias salygas sugalvoti skaiciu 3 skaitmenu.
+// Pavyzdziui: 123 -> 1 + 2 + 3 = 6.
+console.log(Math.floor(123 / 100) + Math.floor((123 % 100) / 10) + (123 % 10));
+console.log('---------------------------------------------------------------------');
+// Dabar pagal tas pacias salygas sugalvoti skaiciu 4 skaitmenu.
+// Pavyzdziui: 1234 -> 1 + 2 + 3 + 4 = 10.
+console.log(Math.floor(1234 / 1000) + Math.floor((1234 % 1000) / 100) + Math.floor((1234 % 100) / 10) + (1234 % 10));
+console.log('---------------------------------------------------------------------');
---
+++ js/taskIFElse.js
@@ -0,0 +1,75 @@
+// uzduosiu (1/2) 1.
+const a = 2;
+const b = 4;
+const c = 10;
+
+// 1. 10 uzduosiu su if else
+// reik parasyt ternary operatoriu
+// if (a == b) {
+//     console.log('a yra lygus b');
+// }
+let result = (a == b) ? 'a yra lygus b' : 'a nera lygus b';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (b == c) {
+//     console.log('b yra lygus c');
+// }
+result = (b == c) ? 'b yra lygus c' : 'b nera lygus c';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (a > b) {
+//     console.log('a yra didesnis uz b');
+// }
+result = (a > b) ? 'a yra didesnis uz b' : 'a nera didesnis uz b';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (b > (c * 2)) {
+//     console.log('b yra didesnis uz c');
+// }
+result = (b > (c * 2)) ? 'b yra didesnis uz c' : 'b nera didesnis uz c';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (a % 2 == 0) {
+//     console.log('a yra lyginis');
+// }
+result = (a % 2 == 0) ? 'a yra lyginis' : 'a nera lyginis';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (b % 2 !== 0) {
+//     console.log('b yra nelyginis');
+// }
+result = (b % 2 !== 0) ? 'b yra nelyginis' : 'b nera nelyginis';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (c > 0) {
+//     console.log('c yra teigiamas');
+// }
+result = (c > 0) ? 'c yra teigiamas' : 'c nera teigiamas';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (a < 0) {
+//     console.log('a yra neigiamas');
+// }
+result = (a < 0) ? 'a yra neigiamas' : 'a nera neigiamas';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (b % 4 == 0) {
+//     console.log('b dalinasi is 4');
+// }
+result = (b % 4 == 0) ? 'b dalinasi is 4' : 'b nesidalina is 4';
+console.log(result);
+console.log('---------------------------------------------------------------------');
+
+// if (c % 8 == 0) {
+//     console.log('c dalinasi is 8');
+// }
+result = (c % 8 == 0) ? 'c dalinasi is 8' : 'c nesidalina is 8';
+console.log(result);
